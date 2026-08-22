27f5361bf1d53477ecd3fb069f8ef3a18088fff6
pihu001488-svg <pihu001488@gmail.com>
2026-08-22 23:59:56 +0530

    Create comments.c

---
+++ comments.c
@@ -0,0 +1,14 @@
+/*Name: Pihu Thakur
+   *Branch:IT
+   *Year:first
+   *Date:22|08|2026
+   *Description: my journey of learning c programming from basic to advance
+     */
+#include<stdio.h>
+
+int main ( ){
+      int a;  //Declared variable 'a'
+       a=6;
+       printf (" The output of this program is %d" , a);
+       return 0;
+}
